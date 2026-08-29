4db3d670d30499c00593e9cf609f6747c28797ec
amans96 <aman1996sisay@gmail.com>
2026-08-29 08:50:56 +0300

    last commit i think

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,13 @@
 import React, { useState } from "react";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import UserProfile from "./UserProfile.jsx";
-
-function App() {
-    const [name, setName] = useState("abc");
-    const [islogged, setIslogged] = useState(false);
-    const [isON, setIsON] = useState(false);
-
-    function Login() {
-        setIslogged(true);
-        setName("abc");
-    }
-
-    function Logout() {
-        setIslogged(false);
-        setName("user x");
-    }
-
-    return (
-        <div 
-            style={{ 
-        
-                backgroundColor: isON ? "#222222" : "#ffffff", 
-                color: isON ? "#ffffff" : "#000000",
-                minHeight: "100vh", 
-                width: "100vw",
-                padding: "20px"
-            }}
-        >
-            {islogged ? (
-                <>
-                    <p>Welcome {name}</p>
-                    <button onClick={Logout}>Logout</button>
-                </>
-            ) : (
-                <>
-                    <p>user please login </p>
-                  <UserProfile/>
-                </>
-            )}
-            
-            <hr style={{ borderColor: isON ? "#444" : "#ccc", margin: "20px 0" }} />
-            
-            <div>
-                <button onClick={() => setIsON(!isON)}>
-                    {isON ? "Dark Mode ON " : "Light Mode OFF "} 
-                </button>
-            </div>
+import Profile from "./profile.jsx";
+import Social from "./social.jsx";
+export default function App() {
+    return(
+        <div>
+            <Profile/>
+            <Social/>
         </div>
     );
 }
-
-export default App;
